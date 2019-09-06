e624d5fe01b8b808f38a7169e184e5b5cbbb7be2
beard <joshuabeard92@gmail.com>
2019-09-06 15:16:11 -0400

    improve setup script

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,5 +1,14 @@
-HERE=pwd
-OPENFRAMEWORKS_ROOT_DIR=~/code/openFrameworks
+HERE=$PWD
+DEFAULT_ROOT_DIR=$HOME
+
+# Where do we put it all?
+read -p "Where do you want to install openFrameworks? (default $DEFAULT_ROOT_DIR): " USER_DEFINED_ROOT_DIR
+
+if [ -z "$USER_DEFINED_ROOT_DIR" ]; then
+    OPENFRAMEWORKS_DIR=$DEFAULT_ROOT_DIR/openFrameworks
+else
+    OPENFRAMEWORKS_DIR=$USER_DEFINED_ROOT_DIR/openFrameworks
+fi
 
 # Install ffmpeg
 sudo apt install ffmpeg
@@ -10,10 +19,12 @@ sudo apt-get update
 sudo apt-get install obs-studio
 
 # Download OF and compile from source
-git clone --recursive git@github.com:openframeworks/openFrameworks.git $OPENFRAMEWORKS_ROOT_DIR
+echo "Downloading and installing to $OPENFRAMEWORKS_DIR"
+echo "This will take a while..."
+git clone --recursive git@github.com:openframeworks/openFrameworks.git $OPENFRAMEWORKS_DIR
 
 # Build stuff
-cd $OPENFRAMEWORKS_ROOT_DIR
+cd $OPENFRAMEWORKS_DIR
 sudo ./scripts/linux/ubuntu/install_dependencies.sh
 sudo ./scripts/linux/ubuntu/install_codecs.sh
 
